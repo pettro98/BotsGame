b7e3aa336fc7f67c13cf52d94dc9e6a385b2974b
GolubkovArtem <ArtemGolubkov@bk.ru>
2017-10-05 21:37:47 +0300

    Update TMap.h

--- arch/TMap.h
+++ arch/TMap.h
@@ -7,33 +7,38 @@
 namespace game_module
 {
 
-	class TMap { // класс карты гексов
+	using size_type = short;
+	using pair = std::pair<size_type, size_type>;
 
-		using size_type = short;
+	class TMap
+	{ 
 
 		THex ** root; // указатель на двумерный массив гексов
+		size_type dimension;
+		std::string map_type;
 
 	public:
 
-		// деструктор, основной конструктор
 		~TMap();
 		TMap(size_type dimension, size_type player_number, const std::string & map_type);
-		//
-
-		// базовые конструкторы, операторы // все ли нужны?
-		TMap();
-		TMap(TMap && map);
-		TMap & operator = (TMap && map);
 		TMap(const TMap & map);
 		TMap & operator = (const TMap & map);
+
+		// базовые конструкторы, операторы
+		TMap() = delete;
+		TMap(TMap && map);
+		TMap & operator = (TMap && map);
 		//
+
+		size_type get_index(const pair & coord) const; // выдаёт индекс гекса
+		size_type get_index(size_type coord_1, size_type coord_2) const;
+		size_type get_dimension() const;
+		size_type get_map_type() const;
 
 	private:
 
+		THex & operator () (const pair & coord); // выдаёт гекс
 		THex & operator () (size_type coord_1, size_type coord_2); // выдаёт гекс
-		//size_type 
-
-		size_type get_index(size_type coord_1, size_type coord_2) const; // выдаёт индекс гекса
 
 		void generate_map( // вспомогательный метод, генерирующий карту, вызывается в конструкторе
 			size_type dimension = 10, size_type player_number = 2,  // dimension - размерность карты,
@@ -45,33 +50,48 @@ namespace game_module
 
 
 
-	class THex  // класс одного гекса (поля)
+	class THex
 	{
 
-		using size_type = short;
-
-		size_type coordinate1; // координата 1
-		size_type coordinate2; // координата 2
+		pair coordinates; // координаты гекса
 		size_type index; // индекс владельца
+		TUnit * unit; // указатель на юнит в гексе 
+		TCapital * capital; // указатель на столицу области
+
 
 	public:
 
-		// деструктор, конструктор
+
 		~THex();
+		THex(THex * phex);
 		THex(size_type coord_1 ,size_type coord_2);
-		//
+		THex & operator = (const THex & phex);
+	
 
 		// получение полей класса
-		size_type get_coordinate1() const;
-		size_type get_coordinate2() const;
+		pair get_coordinates() const;
 		size_type get_index() const;
+		size_type get_capital_district_index() const;
+		size_type get_capital_district_money() const;
 		//
+
+		bool occupied() const; // наличие юнита на гексе
 
 	private:
 
 		// изменение полей класса (координаты не меняются, они определяются при создании обьекта)
-		void change_index(size_type);
+		void change_index(size_type new_index);
+		void change_unit(TUnit * unit); // помещает нового юнита в гекс, старого удаляет
+		bool create_unit(TUnit * unit); // помещает нового юнита в гекс если он не занят
+		void remove_unit(); // разыменовывает указатель
+		void delete_unit(); // вызывает delete для указателя
 		//
+
+		// получение полей класса
+		TUnit * get_unit();
+		TCapital * get_capital();
+		//
+
 
 		friend class TMap;
 	};
