3d6d8feac9cb5326e75c9e3eb275be92573b2446
GolubkovArtem <ArtemGolubkov@bk.ru>
2017-10-18 00:12:05 +0300

    Update and rename main.cpp to Main.cpp

---
+++ arch/Main.cpp
@@ -0,0 +1,19 @@
+#include "Using.h"
+#include "Map.h"
+#include "Unit.h"
+#include "Hex.h"
+#include "IGetData.h"
+#include "ISetData.h"
+#include "IFullAccess.h"
+#include "Game.h"
+#include "Controller.h"
+#include "View.h"
+#include "Player.h"
+
+
+using namespace game_module;
+
+int main() {
+
+
+}
