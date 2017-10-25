5d2d192d8aa31bf1ce94e145480b7b5b44b9742f
pettro98 <ya.skywalker@yandex.ru>
2017-10-25 20:50:22 +0300

    Update Hex.h

--- arch/Hex.h
+++ arch/Hex.h
@@ -1,5 +1,5 @@
-
-
+#include "General.h"
+#include "Unit.h"
 
 
 namespace game_module 
@@ -7,17 +7,13 @@ namespace game_module
 
 	class Hex
 	{
-
 	private:
-
 		pair Coordinates; // координаты гекса
 		size_type Index; // индекс владельца
 		Unit * HexUnit; // указатель на юнит в гексе 
 		Capital * HexCapital; // указатель на столицу области
 
-
 	public:
-
 		~Hex();
 		Hex(Hex * phex);
 		Hex(size_type coord_1, size_type coord_2);
@@ -37,7 +33,6 @@ namespace game_module
 		void remove_hex_unit(); // разыменовывает указатель unit
 		void delete_hex_unit(); // вызывает delete для указателя unit и разыменовывает его
 		void set_hex_unit(Unit * unit); // помещает нового юнита в гекс, старого удаляет
-
 	};
 
 	bool operator != (const Hex & hex1, const Hex & hex2);
