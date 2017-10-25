0a36402d16bd3cc5143359226781cc8a2f570e55
pettro98 <ya.skywalker@yandex.ru>
2017-10-25 20:54:24 +0300

    Update Unit.h

--- arch/Unit.h
+++ arch/Unit.h
@@ -1,3 +1,4 @@
+#include "Hex.h"
 
 
 namespace game_module
@@ -5,13 +6,10 @@ namespace game_module
 
 	class Unit
 	{
-
 	protected:
-
 		Hex * Field; // гекс на котором юнит находится, юнит принадлежит владельцу гекса
 
 	public:
-
 		virtual ~Unit();
 		Unit(Hex * hex); // основной конструктор
 
@@ -26,44 +24,34 @@ namespace game_module
 		bool operator == (const Unit & unit) const;
 
 		void set_hex(Hex * hex); // удаляет также юнита в гексе назначения	
-
 	};
 
-
 	bool operator != (const Unit & unit1, const Unit & unit2);
-
 	
 	Unit * unit_factory(size_type type, size_type strength = 0);
 	// в случае недопустимого значения силы возвращает nullptr
-
+	
 
 	class ActiveUnit
 		: public Unit
 	{
-
 	protected:
-
 		size_type Strength;
 
 	public:
-
 		virtual ~ActiveUnit();
 		ActiveUnit(Hex * hex, size_type strng);
 		size_type strength() const;
-
 	};
 
 
 	class Army
 		: public ActiveUnit
 	{
-
 	private:
-
 		bool Moved;
 		
 	public:
-
 		~Army();
 		Army(Hex * hex, size_type strng);
 		unit_type type();
@@ -73,29 +61,27 @@ namespace game_module
 		void set_strength(size_type strng);
 		void set_moved(bool moved);
 		void die(); // создает могилу на гексе, применяется при нехватке снабжения
-
 	};
 
 
-	struct Tower
+	class Tower
 		: public ActiveUnit
 	{
+	public:
 		~Tower();
 		Tower(Hex * hex, size_type strng);
 		unit_type type();
-
 	};
 
 
 	class Capital
 		: public ActiveUnit
 	{
-
+	private:
 		size_type DistrictIndex;
 		size_type DistrictMoney;
 
 	public:
-
 		~Capital();
 		Capital(Hex * hex);
 		Capital(Capital * capital);
@@ -108,23 +94,23 @@ namespace game_module
 		bool change_district_money(int money);  // прибавляет money к DistrictMoney,
 		// если получается меньше 0, приравнивает DistrictMoney к нулю
 		// и возвращает false
-
 	};
 
 
-
-	struct PassiveUnit
+	class PassiveUnit
 		:public Unit
 	{
+	public:
 		virtual ~PassiveUnit();
 		PassiveUnit(Hex * hex);
 		size_type strength(); // возвращает 0
 	};
 
 
-	struct Farm
+	class Farm
 		: public PassiveUnit
 	{
+	public:
 		~Farm();
 		Farm(Hex * hex);
 		unit_type type();
@@ -135,27 +121,23 @@ namespace game_module
 	class Tree
 		: public PassiveUnit
 	{
-	
 	protected:
-
 		size_type TurnsFromDouble;
 
 	public:
-
 		virtual ~Tree();
 		Tree(Hex * hex);
 		bool ready_to_double() const;
 
 		void has_doubled(); // обнуляет turns_from_double
 		void operator ++(); // ++turns_from_double
-
 	};
 
 
-	struct Palm
+	class Palm
 		: public Tree
 	{
-
+	public:
 		~Palm();
 		Palm(Hex * hex);
 		static size_type turns_to_double(); // возвращает срок за который размножается данный тип
@@ -164,26 +146,24 @@ namespace game_module
 	};
 
 
-	struct Pine
+	class Pine
 		: public Tree
 	{
-
+	public:
 		~Pine();
 		Pine(Hex * hex);
 		static size_type turns_to_double(); // возвращает срок за который размножается данный тип
 		unit_type type();
-
 	};
 
 
-	struct Grave
+	class Grave
 		: public PassiveUnit
 	{
+	public:
 		~Grave();
 		Grave(Hex * hex);
 		unit_type type();
-
 	};
-
 	
 }
