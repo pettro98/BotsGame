5105c283a777b00a6d953ffe0e2d5234fe6a783f
pettro98 <ya.skywalker@yandex.ru>
2017-10-28 17:37:10 +0300

    Update Map.h

--- arch/Map.h
+++ arch/Map.h
@@ -6,40 +6,65 @@
 
 namespace game_module
 {
-
+	template<size_t N>
 	class Map
 	{
 	private:
-		Hex ** Root; // указатель на двумерный массив гексов
-		size_type Dimension; // размерность карты
+		using mas = std::array<std::array<Hex, N>, N>;
+
+		mas Root;		     // двумерный массив гексов
 		std::string MapType; // тип карты
 
 	public:
-		~Map(); // вызывает метод clear
-		Map() = delete;
-		Map(size_type dimension = 10,
-		    size_type player_number = 6,
-		    const std::string & map_type = "classic");
-		Map(const Map & map);
-		Map & operator = (const Map & map);
+		~Map() // вызывает метод clear
+		{
+			clear();
+		}
 
 		// базовые конструкторы, операторы
+		Map(size_type player_number = 6, const std::string & map_type = "classic")
+		{
+			generate_map(player_number, map_type)
+		}
+
+		Map(const Map & map) = delete;
+		Map & operator = (const Map & map) = delete;		//зачем?
+
 		Map(Map && map) = delete;
 		Map & operator = (Map && map) = delete;
 		//
 
-		size_type dimension() const;
-		size_type map_type() const;
+		size_t dimension() const
+		{
+			return N;
+		}
+		std::string map_type() const
+		{
+			return MapType;
+		}
 
-		Hex & operator () (const pair & coord); // выдаёт гекс
-		Hex & operator () (size_type coord_1, size_type coord_2); // выдаёт гекс
-	
-		void clear(); // очищает карту, в том числе удаляет всех юнитов на ней
-	
+		Hex & operator () (const pair & coord)		   // выдаёт гекс
+		{
+			return Root[coord.first][coord.second];
+		}
+		Hex & operator () (size_type coord_1, size_type coord_2) // выдаёт гекс
+		{
+			return Root[coord_1][coord_2];
+		}
+
+		void clear() // очищает карту, в том числе удаляет всех юнитов на ней
+		{
+			for (int i = 0; i < N; ++i)
+				for (int j = 0; j < N; ++j)
+				{
+					Root[i][j].delete_hex_unit();
+				}
+		}
+
 	private:
 		void generate_map( // вспомогательный метод, генерирующий карту, вызывается в конструкторе
-			size_type dimension = 10, size_type player_number = 6,
-			const std::string & map_type = "classic");
+						  size_type player_number = 6,
+						  const std::string & map_type = "classic");
 
 	};
 
