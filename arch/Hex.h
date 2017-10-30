39fc037357d12b7846d6de46c2a819cbac57baaf
GolubkovArtem <ArtemGolubkov@bk.ru>
2017-10-30 20:07:08 +0300

    Update Hex.h

--- arch/Hex.h
+++ arch/Hex.h
@@ -4,43 +4,64 @@
 #include "Unit.h"
 
 
+
 namespace game_module
 {
 
+	class Unit;
+	class Capital;
+
 	class Hex
 	{
+
 	private:
-		pair Coordinates; // координаты гекса
-		size_type Index; // индекс владельца
+
+		Pair Coordinates; // координаты гекса
+		hex_color Index; // индекс владельца
 		Unit * HexUnit; // указатель на юнит в гексе 
 		Capital * HexCapital; // указатель на столицу области
 
+
 	public:
-		~Hex()
-		{
 
-		}
-		Hex();
-		Hex(Hex * phex);
+		~Hex();
+
 		Hex(size_type coord_1, size_type coord_2);
-		Hex & operator = (const Hex & hex);
 
-		pair coordinates() const;
-		size_type index() const;
+		Pair coordinates() const;
+
+		hex_color index() const;
+
+		unit_type get_hex_unit_type() const;
+
 		Unit * get_hex_unit();
+
 		Capital * get_hex_capital();
 
-		bool occupied() const; // наличие юнита в гексе
+		bool occupied() const;
 
 		bool operator == (const Hex & hex) const;
+		
+		void set_index(hex_color new_index);
+		
+		void set_hex_capital(Capital * new_capital);
 
-		void set_index(size_type new_index);
-		void set_hex_capital(Capital * new_capital); // заменяет указатель capital
-		Unit* remove_hex_unit(); // убирает указатель на Unit и разыменовывает его
-		void delete_hex_unit(); // вызывает delete для указателя unit
-		void set_hex_unit(Unit * unit); // помещает нового юнита в гекс, старого удаляет
+		void remove_hex_unit();
+
+		void delete_hex_unit();
+
+		void set_hex_unit(Unit * unit);
+		
 	};
 
 	bool operator != (const Hex & hex1, const Hex & hex2);
+
+	size_type get_distance(const Pair & hex1, const Pair & hex2);
+
+	size_type get_distance(const Hex & hex1, const Hex & hex2);
+
+	std::vector<Pair> get_hex_row(const Pair & hex, size_type radius, size_type max);
+
+	bool hex_exist(const Pair & hex, size_type max);
 
 }
