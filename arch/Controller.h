fb1d643a6c01bdcf54e68a3720b75cdcc88af009
GolubkovArtem <ArtemGolubkov@bk.ru>
2017-10-30 20:03:00 +0300

    Update Controller.h

--- arch/Controller.h
+++ arch/Controller.h
@@ -14,43 +14,46 @@ namespace game_module
 		IFullAccess * MyAccess;
 
 	public:
-		size_type get_index(pair hex) const;
-		size_type distance(pair hex1, pair hex2) const;
-		bool can_move(const Hex & hex1, const Hex & hex2) const;
-		unit_type get_unit_type(pair hex) const;
-		size_type get_strength(pair hex) const;
-		size_type get_capital_index(pair hex) const;
-		bool get_moved(pair hex) const;
-		size_type get_district_money(pair hex) const;
-		size_type get_district_index(pair hex) const;
+
+		~Controller() = default;
+		Controller(IFullAccess * my_access);
+
+		size_type get_index(const Pair & hex) const;
+		size_type distance(const Pair & hex1, const Pair & hex2) const;
+		bool can_move(const Pair & hex1, const Pair & hex2, size_type move_points = Army::move_points()) const;
+		unit_type get_unit_type(Pair hex) const;
+		size_type get_strength(Pair hex) const;
+		bool get_moved(Pair hex) const;
+		size_type get_district_money(Pair hex) const;
+		size_type get_district_index(Pair hex) const;
 		std::string get_map_type() const;
 		size_type get_map_dimension() const;
 		size_type get_players_number() const;
-		bool can_place_static(pair hex) const;
-		bool can_place_army(pair hex, size_type str = 1) const;
-		std::vector<pair> get_neighbours(pair hex) const;
-		std::vector<pair> get_hex_to_capture(pair hex) const;
-		std::vector<pair> get_district_units(pair hex, unit_type type) const;
-		std::vector<pair> get_enemy_br_units(size_type district_index) const;
-		std::vector<pair> get_enemy_inv_armies(size_type district_index) const;
-		std::vector<pair> get_enemy_br_towers(size_type district_index) const;
-		std::vector<pair> get_enemy_br_farms(size_type district_index) const;
-		std::vector<pair> get_district_static(size_type district_index) const;
-		std::vector<pair> get_br_static(size_type district_index) const;
-		std::vector<pair> get_district_palms(size_type district_index) const;
-		std::vector<pair> get_br_palms(size_type district_index) const;
+		bool can_place_static(Pair hex) const;
+		bool can_place_army(Pair hex, size_type str = 1) const;
+		std::vector<Pair> get_neighbours(Pair hex) const;
+		std::vector<Pair> get_hex_to_capture(Pair hex) const;
+		std::vector<Pair> get_district_units(Pair hex, unit_type type) const;
+		std::vector<Pair> get_enemy_br_units(size_type district_index) const;
+		std::vector<Pair> get_enemy_inv_armies(size_type district_index) const;
+		std::vector<Pair> get_enemy_br_towers(size_type district_index) const;
+		std::vector<Pair> get_enemy_br_farms(size_type district_index) const;
+		std::vector<Pair> get_district_static(size_type district_index) const;
+		std::vector<Pair> get_br_static(size_type district_index) const;
+		std::vector<Pair> get_district_palms(size_type district_index) const;
+		std::vector<Pair> get_br_palms(size_type district_index) const;
 
-		bool make_move(pair start, pair end);
-		bool buy_tower(pair hex, size_type strength = 1);
-		bool buy_farm(pair hex);
-		bool buy_army(pair hex, size_type strength = 1);
+		bool make_move(Pair start, Pair end);
+		bool buy_tower(Pair hex, size_type strength = 1);
+		bool buy_farm(Pair hex);
+		bool buy_army(Pair hex, size_type strength = 1);
 
-		size_type get_district_income(pair hex) const;
-		size_type get_district_costs(pair hex) const;
+		size_type get_district_income(Pair hex) const;
+		size_type get_district_costs(Pair hex) const;
 		size_type get_farm_cost(size_type district_index) const;
 		size_type get_army_cost(size_type strength = 1) const;
 		size_type get_tower_cost(size_type strength = 1) const;
-		std::vector<pair> get_army_list() const;
+		std::vector<Pair> get_army_list() const;
 	};
 
 }
